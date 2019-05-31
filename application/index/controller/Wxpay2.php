0bac475c074a836572f3329a94760f73c2e05bb8
lilu <lilusiring@163.com>
2019-05-31 19:47:31 +0800

    push

--- application/index/controller/Wxpay2.php
+++ application/index/controller/Wxpay2.php
@@ -440,7 +440,7 @@ function xmlToArray($xml)
             $s = $this->getSign($data, false);
             $data["sign"] = $s;
             $xml = $this->arrayToXml($data);
-            $response = $this->postXmlCurl2($xml, $url);
+            $response = $this->postXmlCurl3($xml, $url);
             //将微信返回的结果xml转成数组
         //    return $this->xmlstr_to_array($response);
             return $this->xmlToArray($response);
@@ -504,6 +504,8 @@ function xmlToArray($xml)
         //    return $this->xmlstr_to_array($response);
             return $this->xmlToArray($response);
         }
+
+
         public function order_refunds($orderid,$money,$total_fee){
             
             $map = array(
@@ -528,5 +530,70 @@ function xmlToArray($xml)
             //  file_put_contents("/data/wwwroot/ssl.siring.com.cn/ssl/refund.txt",$result);
               return $result;
         }
+        /**
+         * ceshi
+         */
+        /**
+	 * 以post方式提交xml到对应的接口url
+	 * 
+	 * @param string $xml  需要post的xml数据
+	 * @param string $url  url
+	 * @param bool $useCert 是否需要证书，默认不需要
+	 * @param int $second   url执行超时时间，默认30s
+	 * @throws WxPayException
+	 */
+	private static function postXmlCurl3($xml, $url, $useCert = true, $second = 30)
+	{		
+		$ch = curl_init();
+		//设置超时
+		curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+		
+		//如果有配置代理这里就设置代理
+		if(WxPayConfig::CURL_PROXY_HOST != "0.0.0.0" 
+			&& WxPayConfig::CURL_PROXY_PORT != 0){
+			curl_setopt($ch,CURLOPT_PROXY, WxPayConfig::CURL_PROXY_HOST);
+			curl_setopt($ch,CURLOPT_PROXYPORT, WxPayConfig::CURL_PROXY_PORT);
+		}
+		curl_setopt($ch,CURLOPT_URL, $url);
+//		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+//		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+	 /*TODO:防止报60错误，ssl版本限制*/
+        	if(stripos($url,"https://")!==FALSE){
+            		curl_setopt($ch,CURLOPT_SSLVERSION,CURL_SSLVERSION_TLSv1);
+            		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+            		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
+        	}else{
+            		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+            		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+       		 }
+        /*TODO:防止报60错误，ssl版本限制*/
+		//设置header
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		//要求结果为字符串且输出到屏幕上
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	
+		if($useCert == true){
+			//设置证书
+			//使用证书：cert 与 key 分别属于两个.pem文件
+			curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+			curl_setopt($ch,CURLOPT_SSLCERT, '/data/wwwroot/ssl.siring.com.cn/ssl/extend/WxpayAPI/cert/apiclient_cert.pem');
+			curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+			curl_setopt($ch,CURLOPT_SSLKEY,  '/data/wwwroot/ssl.siring.com.cn/ssl/extend/WxpayAPI/cert/apiclient_key.pem');
+		}
+		//post提交方式
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+		//运行curl
+		$data = curl_exec($ch);
+		//返回结果
+		if($data){
+			curl_close($ch);
+			return $data;
+		} else { 
+			$error = curl_errno($ch);
+			curl_close($ch);
+			throw new WxPayException("curl出错，错误码:$error");
+		}
+	}
 
 }
